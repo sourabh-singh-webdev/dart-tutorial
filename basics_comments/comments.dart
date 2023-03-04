3d564e1c82a6c3e1eb16cbbf9df93d3e7aade57f
sourabh_singh <sourabhruhal0999@gmail.com>
2023-03-04 20:11:12 +0530

    comments done

---
+++ basics_comments/comments.dart
@@ -0,0 +1,31 @@
+void main() {
+
+  // This is a single line comment, generally to write for the specific part of code
+  print('The above is the single line comment');
+
+
+  /* This 
+    is
+    a 
+    multiline
+    comment
+  */
+  print('The above is the multiline comment');
+
+
+  /* 
+  Also we can write multiline comment using the triple ///
+  */
+
+
+  /// I am 
+  /// the
+  /// triple
+  /// comment
+  /// in 
+  /// dart
+
+  print('The above is the triple slash commment, it is automatic ');
+
+  
+}
