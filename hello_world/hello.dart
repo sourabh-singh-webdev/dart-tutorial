920dd0203f3db5c0c532eba2243f9a78e271a3d8
sourabh_singh <sourabhruhal0999@gmail.com>
2023-03-05 11:48:23 +0530

    Strings done

--- hello_world/hello.dart
+++ hello_world/hello.dart
@@ -2,6 +2,7 @@ void main() {
 
   // Printing hello world
   print('Hello world');
+  
 
 
 }
---
+++ variables and datatypes/datatypes.dart
@@ -0,0 +1,37 @@
+void main() {
+  // Every datatype in DArt is an object
+  // And undeclared variable in dart are initialised with null
+
+  /// Datatypes in dart are as following:
+  /// 1).Number:---i). int - for number
+  ///             ii). double - for decimals number
+  ///
+  /// Also, we can declare hexagonal number
+
+  int age = 3;
+  double marks = 12.5;
+  print(age);
+  print(marks);
+
+  /// 2).Strings:--- Using the string keyword
+  ///
+
+  String name = 'sourabh singh';
+  print(name);
+
+  /// 3).Boolean:--- true or false
+  ///
+
+  bool isHappy = false;
+  print(isHappy);
+
+  /// 4).Lists:-- Similar to the array, or we can say collection of objects
+  ///
+
+  var toppers = List.generate(3);
+  toppers[0] = 1;
+  toppers[1] = 2;
+  toppers[2] = 3;
+  toppers[3] = 4;
+  print(toppers);
+}
