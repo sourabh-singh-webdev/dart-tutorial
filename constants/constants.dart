7ba72a2efa201835177daeb42d23f7e704773f73
sourabh_singh <sourabhruhal0999@gmail.com>
2023-03-05 11:56:18 +0530

    done

--- constants/constants.dart
+++ constants/constants.dart
@@ -2,10 +2,8 @@ void main() {
   /// In dart we can use two keywords to make our variable constants :
   /// 1). using the "final" keyword: -- It does not preserve any memory until it is not accessed (run time value can be assigned)
   /// 2). using the "const" keyword: -- It preserve memory (compile time value is assigned)
-  /// 
+  ///
   final name = 'sourabh';
-  
+  // can not be reassigned
   const age = '20';
-
-  
 }
